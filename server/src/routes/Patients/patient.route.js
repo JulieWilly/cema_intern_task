c3bcad672cc2f40f7727b73b773a943f77447bae
Wilfred Muchire <logincoin40@gmail.com>
2025-04-26 02:01:09 +0300

    setting up the server, configuring database and creating a connection

---
+++ server/src/routes/Patients/patient.route.js
@@ -0,0 +1,30 @@
+import connection from "../../database";
+
+import { Router } from "express";
+import { Router } from "express";
+const router = Router();    
+
+router.post("/register-client", async (req, res) => { 
+    const { full_name, email_address, phone_number, age, gender, address, national_id, next_of_kin_contact,  medical_history,  current_medication, marital_status,   occupation, addmission_date } = req.body;
+    console.log(req.body);
+
+        try{
+            const query = `INSERT INTO patients (full_name, email_address, phone_number, age,gender, address, national_id, next_of_kin_contact,  medical_history,  current_medication, marital_status,   occupation, addmission_date) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+
+            connection.query(query, [full_name, email_address, phone_number, age,gender, address, national_id, next_of_kin_contact,  medical_history,  current_medication, marital_status,  occupation, addmission_date], (err, result) => {
+            if (err) { 
+                        console.error("Error inserting data: ", err);
+                        return res.status(500).json({ message: "Internal server error" });
+                    }
+                    console.log("Data inserted successfully: ", result);
+                    if (result.affectedRows === 0) {
+                        return res.status(400).json({ message: "Failed to register patient" });                                 
+                    }
+
+                    return res.status(200).json({ message: "Patient registered successfully" });
+        }
+        }catch(err){
+        return res.status(500).json({ message: "Internal server error" });
+      }  
+    
+ });
