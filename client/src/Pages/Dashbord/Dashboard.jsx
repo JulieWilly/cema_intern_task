95c4096949a71b51b1b3b9e7d3dc82e79b3024ef
Wilfred Muchire <logincoin40@gmail.com>
2025-04-26 15:07:07 +0300

    getting and mapping patients data

--- client/src/Pages/Dashbord/Dashboard.jsx
+++ client/src/Pages/Dashbord/Dashboard.jsx
@@ -1,10 +1,35 @@
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { VITE_API_URL_BASE } from "../../config/config";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
     const navigate=useNavigate();
-  const handleNavigate = () =>{
-    navigate("/id/details")
+    const [patients, setPatients] = useState([]);
+
+    const getPatients = async () => {
+      try {
+        const getData = await axios.get(`${VITE_API_URL_BASE}/patient/get-patients`).catch((error) => {
+          console.error("Error fetching patients data:", error);
+        }); 
+        const patients = getData.data.data;
+        if(getData.status === 200) {
+          setPatients(patients);
+        }
+        if(patients.status === 400) {
+          console.log("Failed to fetch patients data", patients.data);
+        }
+      } catch (error) {
+        console.log('Error fetching details', error)
+      }
+    }
+  const handleNavigate = (id) =>{
+    navigate(`/${id}/details`)
   }
+
+  useEffect(() => {
+    getPatients();
+  }, [])
   return (
     <div>
       <h1 className="text-3xl font-bold">Cema Dashboard</h1>
@@ -19,7 +44,7 @@ const Dashboard = () => {
             id="searchInput"
             type="text"
             placeholder="Search..."
-            className="border border-black px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className=" px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
           />
         </div>
         <button
@@ -61,24 +86,29 @@ const Dashboard = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white">
-            <td className="px-4 py-2 border-b">John Doe</td>
-            <td className="px-4 py-2 border-b">jamesBond@gmail.com</td>
-            <td className="px-4 py-2 border-b">+254 223099923</td>
-            <td className="px-4 py-2 border-b">Male</td>
-            <td className="px-4 py-2 border-b">20</td>
-            <td className="px-4 py-2 border-b">299329923</td>
+          {patients &&
+            patients.map((patient, key) => (
 
-            <td className="px-4 py-2 border-b">dd/mm/yyyy</td>
-            <td className="px-2 py-2 border-b  ">
-              <span
-                className="text-sm bg-primary p-2 rounded-sm text-white cursor-pointer"
-                onClick={() => handleNavigate()}
-              >
-                View More
-              </span>
-            </td>
-          </tr>
+                <tr className="bg-white" key={key}>
+                  <td className="px-4 py-2 border-b">{patient.full_name}</td>
+                  <td className="px-4 py-2 border-b">{patient.email_address}</td>
+                  <td className="px-4 py-2 border-b">+254 {patient.phone_number}</td>
+                  <td className="px-4 py-2 border-b">{patient.gender}</td>
+                  <td className="px-4 py-2 border-b">{patient.age}</td>
+                  <td className="px-4 py-2 border-b">{patient.national_id}</td>
+
+                  <td className="px-4 py-2 border-b">{patient.admission_date}</td>
+                  <td className="px-2 py-2 border-b  ">
+                    <span
+                      className="text-sm bg-primary p-2 rounded-sm text-white cursor-pointer"
+                      onClick={() => handleNavigate(patient.id)}
+                    >
+                      View More
+                    </span>
+                  </td>
+                </tr>
+      
+            ))}
         </tbody>
       </table>
     </div>
