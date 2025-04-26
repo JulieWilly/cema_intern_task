82dbf329d77a82b6fdef98ce213ce4def0e872a4
Wilfred Muchire <logincoin40@gmail.com>
2025-04-26 22:38:34 +0300

    setting up the search button for search functionality

--- client/src/Pages/Dashbord/Dashboard.jsx
+++ client/src/Pages/Dashbord/Dashboard.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { VITE_API_URL_BASE } from "../../config/config";
 import { useEffect, useState } from "react";
+import { useFormik } from "formik";
 
 const Dashboard = () => {
     const navigate=useNavigate();
@@ -30,6 +31,26 @@ const Dashboard = () => {
   useEffect(() => {
     getPatients();
   }, [])
+
+  const handleSubmit = async (values) => {
+
+    console.log("Search input:", values);
+    const searchPatient = await axios.get(
+      `${VITE_API_URL_BASE}/search-patients`, {
+        params: {
+          searchInput: values.search_input.trim() ,
+        },
+      }
+    );
+    const patients = searchPatient.data.data;
+    console.log('data', patients);
+  }
+  const formik = useFormik({
+    initialValues:{
+      searchInput: "",
+    },
+    onSubmit: handleSubmit
+  });
   return (
     <div>
       <h1 className="text-3xl font-bold">Cema Dashboard</h1>
@@ -38,22 +59,24 @@ const Dashboard = () => {
       <h2 className=" pb-4 text-2xl tracking-wide font-semibold">
         Patients Details
       </h2>
-      <div className="mb-4 flex items-center">
-        <div className="">
-          <input
-            id="searchInput"
-            type="text"
-            placeholder="Search..."
-            className=" px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          />
+      <form action="" onSubmit={formik.handleSubmit}>
+        <div className="mb-4 flex items-center">
+          <div className="">
+            <input
+              id="search_input"
+              type="text"
+              placeholder="Search..."
+              name="search_input"
+              onChange={formik.handleChange}
+              value={formik.values.search_input}
+              className=" px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            />
+          </div>
+          <button className="ml-2 py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
+            Search
+          </button>
         </div>
-        <button
-          type="button"
-          className="ml-2 py-2 px-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        >
-          Search
-        </button>
-      </div>
+      </form>
 
       <table className="min-w-full table-auto border-collapse">
         <thead>
@@ -88,26 +111,26 @@ const Dashboard = () => {
         <tbody>
           {patients &&
             patients.map((patient, key) => (
+              <tr className="bg-white" key={key}>
+                <td className="px-4 py-2 border-b">{patient.full_name}</td>
+                <td className="px-4 py-2 border-b">{patient.email_address}</td>
+                <td className="px-4 py-2 border-b">
+                  +254 {patient.phone_number}
+                </td>
+                <td className="px-4 py-2 border-b">{patient.gender}</td>
+                <td className="px-4 py-2 border-b">{patient.age}</td>
+                <td className="px-4 py-2 border-b">{patient.national_id}</td>
 
-                <tr className="bg-white" key={key}>
-                  <td className="px-4 py-2 border-b">{patient.full_name}</td>
-                  <td className="px-4 py-2 border-b">{patient.email_address}</td>
-                  <td className="px-4 py-2 border-b">+254 {patient.phone_number}</td>
-                  <td className="px-4 py-2 border-b">{patient.gender}</td>
-                  <td className="px-4 py-2 border-b">{patient.age}</td>
-                  <td className="px-4 py-2 border-b">{patient.national_id}</td>
-
-                  <td className="px-4 py-2 border-b">{patient.admission_date}</td>
-                  <td className="px-2 py-2 border-b  ">
-                    <span
-                      className="text-sm bg-primary p-2 rounded-sm text-white cursor-pointer"
-                      onClick={() => handleNavigate(patient.id)}
-                    >
-                      View More
-                    </span>
-                  </td>
-                </tr>
-      
+                <td className="px-4 py-2 border-b">{patient.admission_date}</td>
+                <td className="px-2 py-2 border-b  ">
+                  <span
+                    className="text-sm bg-primary p-2 rounded-sm text-white cursor-pointer"
+                    onClick={() => handleNavigate(patient.id)}
+                  >
+                    View More
+                  </span>
+                </td>
+              </tr>
             ))}
         </tbody>
       </table>
